eb496b82caae49a64f087e8b74b7de4e04a5e54a
VittoriaBasile <vichy.basile@gmail.com>
2023-06-26 10:51:03 +0200

    struttura base

---
+++ src/main/java/EPICODE/EPIC_BNB/EpicBnbApplication.java
@@ -0,0 +1,13 @@
+package EPICODE.EPIC_BNB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicBnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicBnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EPICODE/EPIC_BNB/EpicBnbApplicationTests.java
@@ -0,0 +1,13 @@
+package EPICODE.EPIC_BNB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicBnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
